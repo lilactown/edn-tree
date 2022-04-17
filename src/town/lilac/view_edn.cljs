9a4b001757f0cad3a7aa940e5072805e45c38220
Will Acton <will@willacton.com>
2022-04-17 13:14:17 -0700

    start working on components in the REPL

--- src/town/lilac/view_edn.cljs
+++ src/town/lilac/view_edn.cljs
@@ -1,7 +1,44 @@
 (ns town.lilac.view-edn
   (:require
-   [helix.core :refer [$ defnc]]))
+   [helix.core :refer [$ defnc]]
+   [helix.dom :as d]))
 
 
-(defnc viewer
-  [{:keys [data]}])
+(declare view)
+
+
+(defnc map-view
+  [{:keys [data]}]
+  (d/div
+   (for [[k v] data]
+     (d/div 
+      {:key (str (hash k) (hash v))}
+      ($ view {:data k})
+      ($ view {:data v})))))
+
+
+(defnc list-view
+  [{:keys [data]}]
+  (d/div
+   (for [v data]
+     (d/div
+      {:key (hash v)}
+      ($ view {:data v})))))
+
+
+(defnc view
+  [{:keys [data]}]
+  (cond
+    (map? data) ($ map-view {:data data})
+    (coll? data) ($ list-view {:data data})
+    :else (str data)))
+
+(comment
+  (require '["react-dom" :as rdom])
+  (def root (rdom/createRoot (js/document.getElementById "app")))
+
+  (.render root (d/div "hi"))
+  
+  (.render root ($ view {:data {:foo "bar" :baz [1 2 3 (range 4 10)]}}))
+  )
+  
