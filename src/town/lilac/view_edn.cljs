d593015948f24c24ae2115c0bbb82d9af9b6ba42
Will Acton <will@willacton.com>
2022-04-23 12:19:16 -0700

    switch to ul/li with aria

--- src/town/lilac/view_edn.cljs
+++ src/town/lilac/view_edn.cljs
@@ -1,89 +1,100 @@
 (ns town.lilac.view-edn
   (:require
-   [helix.core :refer [$ defnc]]
+   [helix.core :refer [$ <> defnc]]
    [helix.dom :as d]
    [helix.hooks :as hooks]))
 
 
 (declare view)
 
-(defnc toggle
-  [{:keys [expanded? on-change]}]
-  (d/label
-   {:class ["town_lilac_view-edn__caret"
-            (when expanded?
-              "town_lilac_view-edn__caret__expanded")]}
-   (d/input
-    {:type "checkbox"
-     :checked expanded?
-     :on-change (when on-change
-                  #(on-change (.. % -target -checked)))})))
+
+(defnc map-entry-view
+  [{:keys [k v]}]
+  (let [[expanded? set-expanded] (hooks/use-state false)]
+    (d/li
+     {:role "treeitem"
+      :aria-expanded? expanded?
+      :on-click #(do (.stopPropagation %)
+                     (set-expanded not))}
+     (d/ul
+      {:class "town_lilac_view-edn__view"
+       :role "group"}
+      ($ view {:data k})
+      ($ view {:data v})))))
 
 
 (defnc map-view
   [{:keys [data initial-realized?]}]
   (let [[realized? set-realized] (hooks/use-state initial-realized?)
         [expanded? set-expanded] (hooks/use-state false)]
-    (d/div
-     {:class ["town_lilac_view-edn__view"]
-      :on-click #(set-realized true)}
-     ($ toggle
-        {:expanded? expanded?
-         :on-change #(set-expanded %)})
-     (d/span {:class "town_lilac_view-edn__map_begin"} "{")
-     (d/div
-      {:class ["town_lilac_view-edn__view-coll"
-               (when expanded?
-                 "town_lilac_view-edn__view-coll__expanded")]}
+    (d/li
+     {:role "treeitem"
+      :aria-expanded expanded?
+      :on-click #(do (.stopPropagation %)
+                     (set-expanded not))}
+     (d/ul
+      {:class ["town_lilac_view-edn__view"]
+       :role "group"
+       :on-click #(do (.stopPropagation %)
+                      (set-realized true))}
+      (d/span {:class "town_lilac_view-edn__map_begin"} "{")
       (if realized?
         (for [[k v] data]
-         (d/div
-          {:key (str (hash k) (hash v))
-           :class "town_lilac_view-edn__view-key-value"}
-          ($ view {:data k :realized? true})
-          ($ view {:data v})))
-        "..."))
-     (d/span {:class "town_lilac_view-edn__map_end"} "}"))))
+          ($ map-entry-view {:key (str (hash k) (hash v))
+                             :k k
+                             :v v}))
+        "...")
+      (d/span {:class "town_lilac_view-edn__map_end"} "}")))))
 
 
 (defnc list-view
-  [{:keys [data initial-realized?]}]
+  [{:keys [data initial-realized? root?]}]
   (let [[begin end] (if (vector? data) "[]" "()")
         [realized? set-realized] (hooks/use-state initial-realized?)
         [expanded? set-expanded] (hooks/use-state false)]
-    (d/div
-     {:class ["town_lilac_view-edn__view"]
-      :on-click (fn [e]
-                  (.stopPropagation e)
-                  (set-realized true))}
-     ($ toggle
-        {:expanded? expanded?
-         :on-change #(set-expanded (.. % -target -checked))})
-     (d/span {:class "town_lilac_view-edn__list_begin"} begin)
-     (d/div
-      {:class ["town_lilac_view-edn__view-coll"
-               (when expanded?
-                 "town_lilac_view-edn__view-coll__expanded")]}
+    (d/li
+      {:role "treeitem"
+       :aria-expanded expanded?
+       :on-click #(do (.stopPropagation %)
+                      (set-expanded not))}
+     (d/ul
+      {:class ["town_lilac_view-edn__view"]
+       :role "group"
+       :on-click (fn [e]
+                   (.stopPropagation e)
+                   (set-realized true))}
+      (d/span {:class "town_lilac_view-edn__list_begin"} begin)
       (if realized?
         (for [v data]
-          (d/div
-           {:key (hash v)}
-           ($ view {:data v})))
-        "..."))
-     (d/span {:class "town_lilac_view-edn__list_begin"} end))))
+          ($ view {:key (hash v) :data v}))
+        "...")
+      (d/span {:class "town_lilac_view-edn__list_begin"} end)))))
 
 
 (defnc view
-  [{:keys [data]}]
+  [{:keys [data root?]}]
   (cond
-    (map? data) ($ map-view {:data data})
-    (coll? data) ($ list-view {:data data})
-    (string? data) (d/span
-                    {:class "town_lilac_view-edn__view"}
-                    "\"" data "\"")
-    :else (d/span
-           {:class "town_lilac_view-edn__view"}
-           (str data))))
+    (map? data) ($ map-view {:data data :root? root?})
+    (coll? data) ($ list-view {:data data :root? root?})
+    (string? data) (d/li
+                    {:role "none"}
+                    (d/span
+                     {:class "town_lilac_view-edn__view"}
+                     "\"" data "\""))
+    :else (d/li
+           {:role "none"}
+           (d/span
+            {:class "town_lilac_view-edn__view"}
+            (str data)))))
+
+
+(defnc root-view
+  [{:keys [data]}]
+  (d/ul
+   {:class "town_lilac_view-edn__view"
+    :role "tree"}
+   ($ view {:data data})))
+
 
 (comment
   ;; setup body
@@ -111,5 +122,5 @@
 
   (.render root (d/div "hi"))
 
-  (.render root ($ view {:data {:foo "bar" :baz [1 2 3 (range 4 10)]}}))
+  (.render root ($ root-view {:data {:foo "bar" :baz [1 2 3 (range 4 10)]}}))
   )
