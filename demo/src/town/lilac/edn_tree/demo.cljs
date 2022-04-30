a503986fbe4670c8ce565879bafca5238b7229f4
Will Acton <will@willacton.com>
2022-04-30 15:31:26 -0700

    demo: modify data in textarea

--- demo/src/town/lilac/edn_tree/demo.cljs
+++ demo/src/town/lilac/edn_tree/demo.cljs
@@ -1,5 +1,6 @@
 (ns town.lilac.edn-tree.demo
   (:require
+   [clojure.edn :as edn]
    [helix.core :refer [$ defnc]]
    [helix.dom :as d]
    [helix.hooks :as hooks]
@@ -7,23 +8,36 @@
    ["react-dom/client" :as rdom]))
 
 
+(defnc app 
+  []
+  (let [[edn-str set-edn-str] (hooks/use-state
+                               #(str {:foo #{"bar"}
+                                      :baz [1 2 3
+                                            {:arst {'neio "foo"}}
+                                            (range 4 10)]}))]
+    (d/div
+     (d/div
+      ($ edn-tree/root
+         {:data (edn/read-string edn-str)
+          :on-click #(prn "clicked" %2)
+          :on-realize #(prn "realized" %2)
+          :on-expand #(prn "expanded" %2)
+          :on-focus #(prn "focus" %2)
+          :on-blur #(prn "blur" %2)}))
+     (d/div 
+      (d/textarea
+       {:value edn-str
+        :cols 80
+        :rows 10
+        :on-change #(set-edn-str (.. % -target -value))})))))
+
+
 (defonce root nil)
 
 
 (defn ^:dev/after-load render-app
   []
-  (.render root
-           ($ edn-tree/tree
-              {:data {:foo #{"bar"}
-                      :baz [1 2 3
-                            {:arst {'neio (ex-info "foo" {})}}
-                            (range 4 10)]}
-               :initial-realize true
-               :on-click #(prn "clicked" %2)
-               :on-realize #(prn "realized" %2)
-               :on-expand #(prn "expanded" %2)
-               :on-focus #(prn "focus" %2)
-               :on-blur #(prn "blur" %2)})))
+  (.render root ($ app)))
 
 (defn ^:export start
   []
